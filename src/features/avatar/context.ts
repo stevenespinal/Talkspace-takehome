9f1a2b580bddcd1b7ee8bd3e6ca30b33cbaf20cb
Steven Espinal <stevenjesusespinal@gmail.com>
2025-03-03 01:42:45 -0500

    refactor(context): App component
    - Extract avatar creator ui into a separate file from app.tsx
    - Add updateName and saveAvatar functions to context providers
    - Update context types to include new handler functions

--- src/features/avatar/context.ts
+++ src/features/avatar/context.ts
@@ -4,16 +4,19 @@ import { AvatarOptions, AvatarList } from '@/features/avatar/types';
 interface AvatarContextType {
   avatarOptions: AvatarOptions;
   setAvatarOptions: (options: AvatarOptions) => void;
+  updateName: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 interface AvatarListContextType {
   avatarList: AvatarList;
   setAvatarList: (list: AvatarList) => void;
+  saveAvatar: (url: string, name: string) => void;
 }
 
 export const AvatarContext = createContext<AvatarContextType>({
   avatarOptions: undefined,
   setAvatarOptions: () => {},
+  updateName: () => {},
 });
 
 export const AvatarURLContext = createContext<string>('');
@@ -21,4 +24,5 @@ export const AvatarURLContext = createContext<string>('');
 export const AvatarListContext = createContext<AvatarListContextType>({
   avatarList: [],
   setAvatarList: () => {},
+  saveAvatar: () => {},
 });
